2de39b69d6ee4fc28e2a39185e5d1af32380d1df
Michal Conos <michal.conos@oracle.com>
2022-11-27 13:04:34 +0100

    zip distro for windows

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target x86_64-pc-windows-gnu
+
+# create tarball for windows
+rm -f rustvaders-x86_64-pc-windows.tar.gz
+cp -av sprites target/x86_64-pc-windows-gnu/release
+tar czvf rustvaders-x86_64-pc-windows.tar.gz target/x86_64-pc-windows-gnu/release
+rm -rf target/x86_64-pc-windows-gnu/release/sprites
