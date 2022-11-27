1982ac6eb5c44a90d6785fd770fcbcc45d4965a6
Michal Conos <michal.conos@oracle.com>
2022-11-27 21:53:59 +0100

    proper logic for bgm audio

--- src/sfx.rs
+++ src/sfx.rs
@@ -8,19 +8,16 @@ const BGM: &str = "BGM";
 
 pub struct Sfx {
     _audio: Audio,
-    _have_bgm: bool,
 }
 
 impl Sfx {
     pub fn new() -> Self {
         let mut _audio = Audio::new();
-        let _have_bgm = std::path::Path::new("sfx/running-against-clock.mp3").exists();
+       
         _audio.add(LASER, "sfx/laser.mp3");
         _audio.add(ALIEN_MISSILE, "sfx/alien_missile.mp3");
-        if _have_bgm {
-            _audio.add(BGM, "sfx/running-against-clock.mp3");
-        }
-        Self { _audio, _have_bgm }
+       
+        Self { _audio, }
     }
 
     pub fn laser(&mut self) {
@@ -34,8 +31,12 @@ impl Sfx {
     pub fn bgm(&mut self) {
         thread::spawn(move || {
             let mut audio = Audio::new();
+            let have_bgm = std::path::Path::new("sfx/running-against-clock.mp3").exists();
             // I don't have rights to distribute this sound
             // Elliot Holmes - Running Against the Clock - https://www.epidemicsound.com/track/8bc4l5LWYf/
+            if !have_bgm {
+                return ;
+            }
             audio.add(BGM, "sfx/running-against-clock.mp3");
             loop {
                 audio.play(BGM);
